4d3ab73bfdd75f9cc116709dc03239a8d435c13f
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-07-25 20:06:19 +0200

    docs: document simple producer

--- src/main/java/no/sysco/middleware/workshop/kafka/producer/SimpleProducerApp.java
+++ src/main/java/no/sysco/middleware/workshop/kafka/producer/SimpleProducerApp.java
@@ -10,27 +10,35 @@ import java.io.IOException;
 import java.util.Properties;
 
 /**
- * Hello world!
+ * Simple Kafka Producer application
  */
 public class SimpleProducerApp {
 
   private final KafkaProducer<String, String> kafkaProducer;
 
   private SimpleProducerApp() {
+    // Setting Producer configurations
     final Properties producerConfigs = new Properties();
     producerConfigs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CommonProperties.BOOTSTRAP_SERVERS);
     producerConfigs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     producerConfigs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
+    // Create Producer
     kafkaProducer = new KafkaProducer<>(producerConfigs);
   }
 
+  /**
+   * Send a producer record
+   */
   private void sendRecord() {
     final ProducerRecord<String, String> record =
         new ProducerRecord<>("simple-topic", "hello-world!");
     kafkaProducer.send(record);
   }
 
+  /**
+   * Run Producer application
+   */
   public static void main(String[] args) throws IOException {
     final SimpleProducerApp simpleProducerApp = new SimpleProducerApp();
     simpleProducerApp.sendRecord();
