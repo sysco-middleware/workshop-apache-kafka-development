8cc3079a36e5fe1f149d49a0545425cb1b230132
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-07-25 20:27:41 +0200

    docs: document batching producer

--- src/main/java/no/sysco/middleware/workshop/kafka/producer/ProducerOneByOneApp.java
+++ src/main/java/no/sysco/middleware/workshop/kafka/producer/ProducerOneByOneApp.java
@@ -9,11 +9,21 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import java.io.IOException;
 import java.util.Properties;
 
-public class ProducerWithoutBatchingApp {
+/**
+ * Creates a Producer to send messages to Kafka one by one, without Batching, or holding messages
+ * in memory.
+ *
+ * This is not recommended if you are trying to achieve high throughput, as it will send each
+ * message individually.
+ *
+ * It will reuse the same topic, or create it.
+ */
+public class ProducerOneByOneApp {
 
   private final KafkaProducer<String, String> kafkaProducer;
 
-  private ProducerWithoutBatchingApp() {
+  private ProducerOneByOneApp() {
+    // Setting Producer configs
     final Properties producerConfigs = new Properties();
     producerConfigs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CommonProperties.BOOTSTRAP_SERVERS);
     producerConfigs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -30,9 +40,13 @@ public class ProducerWithoutBatchingApp {
     kafkaProducer.flush();
   }
 
+  /**
+   * Run Producer application
+   */
   public static void main(String[] args) throws IOException {
-    final ProducerWithoutBatchingApp producerWithAckApp = new ProducerWithoutBatchingApp();
+    final ProducerOneByOneApp producerWithAckApp = new ProducerOneByOneApp();
     producerWithAckApp.sendRecord();
+
     System.out.println("Press ENTER to exit the system");
     System.in.read();
   }
--- src/main/java/no/sysco/middleware/workshop/kafka/producer/ProducerWithBatchingApp.java
+++ src/main/java/no/sysco/middleware/workshop/kafka/producer/ProducerWithBatchingApp.java
@@ -11,6 +11,11 @@ import java.time.Duration;
 import java.util.Properties;
 import java.util.stream.IntStream;
 
+/**
+ * Creates a Producer to send messages that will be batched first by size: trying to hold 100KB
+ * in memory before sending to Kafka, and then if this is taking too long, it will wait for 10 sec.
+ * for this. It will send the batch every 10 secs if no 100KB are batched.
+ */
 public class ProducerWithBatchingApp {
 
   private final KafkaProducer<String, String> kafkaProducer;
@@ -30,11 +35,12 @@ public class ProducerWithBatchingApp {
   private void sendRecord() {
     final ProducerRecord<String, String> record =
         new ProducerRecord<>("simple-topic", "record");
-    kafkaProducer.send(record, (metadata, exception) -> {
-      System.out.println("Ack received");
-    });
+    kafkaProducer.send(record, (metadata, exception) -> System.out.println("Ack received"));
   }
 
+  /**
+   * Run Producer Application
+   */
   public static void main(String[] args) throws IOException {
     final ProducerWithBatchingApp producerWithAckApp = new ProducerWithBatchingApp();
     IntStream.range(0, 1000).forEach(ignored -> producerWithAckApp.sendRecord());
